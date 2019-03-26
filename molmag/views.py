e5f7b547ae501f5aa06b20ed7b1b7265a506d609
Jonathan Bussing <jbussing@ucsd.edu>
2019-03-26 11:57:45 -0700

    Update views.py

--- molmag/views.py
+++ molmag/views.py
@@ -4,40 +4,14 @@ class HomePageView(TemplateView):
     template_name = "index.html"
 
 class AboutPageView(TemplateView):
-
-
- 
-
-
-    
+ 
 	template_name = "about.html"
 
-    
-
-    
-
-
-
 class SearchPageView(TemplateView):
-
-
- 
-
-
-    
+ 
 	template_name = "search.html"
-
-    
-
 
 
 class DocumentationPageView(TemplateView):
 
-
- 
-
-
-    
 	template_name = "documentation.html"
-
- 
