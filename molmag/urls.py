2d2165d098e57da157b51472cbf7536ecdf4c359
Jonathan Bussing <jbussing@ucsd.edu>
2019-03-26 10:47:33 -0700

    Updated urls.py to include AboutPageView

--- molmag/urls.py
+++ molmag/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from molmag.views import HomePageView
+from molmag.views import HomePageView, AboutPageView
 
 urlpatterns = [
 	path('', HomePageView.as_view()),
