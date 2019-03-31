5057a95db023edb8ac2c56868438ae2c7dea190f
jhilgar <jhilgar@ucsd.edu>
2019-03-30 19:34:40 -0700

    Updated Compound model

--- datastore/models.py
+++ datastore/models.py
@@ -28,12 +28,13 @@ class Compound(models.Model):
     checker = models.ForeignKey(User, editable=False, on_delete=models.SET_NULL, null=True, blank=True, related_name='checker')
     doi = models.ForeignKey(Reference, on_delete=models.CASCADE)
     formula = models.CharField('Formula', max_length=50)
-    ccdc = models.CharField('CCDC Identifier', max_length=9)
+    ccdc = models.CharField('CCDC Identifier', max_length=9, null=True, blank=True)
     dimensionality = models.IntegerField(default=0, validators=[MaxValueValidator(3), MinValueValidator(0)])
     ueff = models.FloatField('Effective Barrier', validators=[MaxValueValidator(10000), MinValueValidator(0)], null=True, blank=True)
     hdc = models.FloatField('DC Field', validators=[MaxValueValidator(70000), MinValueValidator(0)], null=True, blank=True)
+    tau0 = models.FloatField('τ₀', null=True, blank=True)
     tc = models.FloatField('Curie Temperature', validators=[MaxValueValidator(10000), MinValueValidator(0)], null=True, blank=True)
-    info = JSONField(default=default_compound_info)
+    info = JSONField(default=default_compound_info, null=True, blank=True)
 
     def __str__(self):
-        return str(self.doi) + ' - ' + self.info['ccdc']
+        return str(self.doi) + ' - ' + str(self.formula)
